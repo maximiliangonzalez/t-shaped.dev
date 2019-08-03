35e14ec9fe7a3fce5267d8839887fee4c4b1cbba
Maximilian Gonzalez <thamaxlg@gmail.com>
2019-08-03 14:05:28 -0700

    restarting app

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+
+import React, {useEffect} from 'react';
+
+const App = () => {
+  return (
+  <>
+    <h1>title</h1>
+  </>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000'
+    }
+  }
+}
