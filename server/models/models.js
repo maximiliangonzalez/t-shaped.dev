c1bdeaedb14e2f8ed9d6e46e909a91e03ecf9363
Maximilian Gonzalez <thamaxlg@gmail.com>
2019-08-16 17:31:46 -0700

    slighly refactored user schema to better facilitate users changing their comfort level w/ topics (MAKE SURE THIS DIDN'T BREAK ANYTHING)

--- server/models/models.js
+++ server/models/models.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     type: [
       {
         topic: {type: mongoose.Schema.ObjectId, ref: 'Topic'},
-        confidence: Number
+        confidence: {type: Boolean, default: false}
       }
     ],
     default: []
