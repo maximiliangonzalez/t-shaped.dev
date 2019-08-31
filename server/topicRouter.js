e924656c6ebf7ea8748d45e85d28acb16791db58
Jaime Gonnzalez <2016jgonzalez139@gmail.com>
2019-08-31 12:37:18 -0700

    restored deleted files

---
+++ server/topicRouter.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { searchTopic, addTopic, followTopic, topicName, findTopic} = require('./controllers/topicController');
+const { checkToken } = require('./controllers/tokenController');
+
+router.get('/:topic', searchTopic, (req, res) => {
+  res.status(200).json(res.locals.topics);
+});
+
+router.get('/topicName/:topic', topicName, (req, res) => {
+  res.status(200).json(res.locals.topic);
+});
+
+router.post('/:topic', checkToken, searchTopic, addTopic, followTopic, (req, res) => {
+  res.status(200).json(res.locals.topic._id);
+});
+
+router.post('/follow/:topic', checkToken, findTopic, followTopic, (req, res) => {
+  res.status(200).json(res.locals.topic._id);
+});
+
+
+
+module.exports = router;
